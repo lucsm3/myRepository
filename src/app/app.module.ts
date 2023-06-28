0fc7c9c69bacbdeb62559f40b0414d7da8f49abd
macbook <macbook@Lucas-m-Macbook.local>
2023-06-27 22:07:29 -0300

    structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
+import { DashboardModule } from './dashboard/dashboard/dashboard.module';
+import { HttpClientModule } from '@angular/common/http'; 
 
 @NgModule({
   declarations: [
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DashboardModule,
+    HttpClientModule  
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    fontSize: {
+      sm: ['25px', '25px'],
+      xs: ['60px', '60px']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
